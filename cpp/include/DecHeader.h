bf5b384461d3371284f12edac3d080dcdf895aa9
Shivam Verma <shivamverma5022@gmail.com>
2025-04-07 00:22:26 +0530

    nits2

--- cpp/include/DecHeader.h
+++ cpp/include/DecHeader.h
@@ -1,15 +1,15 @@
 #pragma once
 
 
-// #include "../include/NvDecoder.h"
-// // #include "../include/NvCodecUtils.h"
-// #include "../include/FFmpegDemuxer.h"
-// // #include "../include/ColorSpace.h"
-// // #include "../include/AppDecUtils.h"
+#include "../include/NvDecoder.h"
+// #include "../include/NvCodecUtils.h"
+#include "../include/FFmpegDemuxer.h"
+// #include "../include/ColorSpace.h"
+// #include "../include/AppDecUtils.h"
 
-// #include "FFmpegDemuxer.h"
-// #include "NvDecoder.h"
-// #include <memory>
+#include "FFmpegDemuxer.h"
+#include "NvDecoder.h"
+#include <memory>
 
 
 enum OutputFormat { native = 0, bgra, bgra64 };
@@ -25,21 +25,21 @@ public:
 
   char *getNext();
 
-// private:
-//     std::unique_ptr<FFmpegDemuxer> demuxer;
-//     std::unique_ptr<NvDecoder> dec;
-//     CUcontext cuContext = NULL;
+private:
+    std::unique_ptr<FFmpegDemuxer> demuxer;
+    std::unique_ptr<NvDecoder> dec;
+    CUcontext cuContext = NULL;
 
-//     Rect cropRect = {};
-//     Dim resizeDim = {};
-//     unsigned int opPoint = 0;
-//     bool bDispAllLayers = false;
-//     bool bExtractUserSEIMessage = false;
-//     unsigned int decsurf = 0;
+    Rect cropRect = {};
+    Dim resizeDim = {};
+    unsigned int opPoint = 0;
+    bool bDispAllLayers = false;
+    bool bExtractUserSEIMessage = false;
+    unsigned int decsurf = 0;
 
 
-//     int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0;
-//     uint8_t *pVideo = NULL, *pFrame;
-//     bool bDecodeOutSemiPlanar = false;
-//     CUstream cuStream = NULL;
+    int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0;
+    uint8_t *pVideo = NULL, *pFrame;
+    bool bDecodeOutSemiPlanar = false;
+    CUstream cuStream = NULL;
 };
